a2f8a5c65c735755228ca633e2b18b7fa4230744
Tej Charfi <charfi@usc.edu>
2020-06-20 20:51:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function theBeatlesPlay(mus, inst){
   var list = [] 
   
   for(var i=0; i< musicians.length; i++){
-    list.push(mus[i] + "" + inst[i])
+    list.push(mus[i] + "plays" + inst[i])
     
   }
   
